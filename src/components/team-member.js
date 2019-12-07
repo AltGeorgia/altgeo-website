cfeb7135b914566c4acb348d7605f3dd51da9cad
Nikaoto <nikaoto@gmail.com>
2019-12-07 22:32:16 +0400

    improve team-member styles and alignments, add responsiveness to team-members

--- src/components/team-member.js
+++ src/components/team-member.js
@@ -4,16 +4,18 @@ import "./team-member.css"
 export default (props) => {
   return (
     <article className={"team-member"}>
-      <img className={"team-member-image"}
-           src={props.image}
-           alt={props.fullName} />
-      <main>
+      <div className={"team-member-left-side"}>
+        <img className={"team-member-image"}
+             src={props.image}
+             alt={props.fullName} />
         <h1>
           {props.fullName}
         </h1>
         <small className={"team-member-role"}>
           {props.role}
         </small>
+      </div>
+      <main>
         <p>
           {props.bio}
         </p>
