9c2f38b28dbb1a3224920971bf8b1cc758d60bf0
Nikaoto <nikaoto@gmail.com>
2019-12-12 11:41:19 +0400

    remove unused sharp & image dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,24 +58,10 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
           `gatsby-remark-copy-linked-files`,
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
